856f3bc5d8001517ecf05f235b49d1c2bb725760
r-tarnowski <radek.github@wp.pl>
2023-02-11 20:10:32 +0100

    Initial commit

---
+++ src/FactoryMethod.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+void printHeader() {
+   cout << endl;
+   cout << "================================================" << endl;
+   cout << "Design Patterns in Modern C++ by Dmitri Nesteruk" << endl;
+   cout << "Factories, Factory Method                        "<< endl;
+   cout << "================================================" << endl;
+   cout << endl;
+}
+
+
+
+int main(int argc, char *argv[]) {
+   printHeader();
+   
+   cout << endl;
+   return 0;
+}
