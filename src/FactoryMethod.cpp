e752cecfab2b03a6248116354027df7e44eaccb3
r-tarnowski <radek.github@wp.pl>
2023-02-12 01:01:31 +0100

    Example of Factory Method design pattern added

--- src/FactoryMethod.cpp
+++ src/FactoryMethod.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <math.h>
 
 using std::cout;
 using std::endl;
@@ -12,10 +13,40 @@ void printHeader() {
    cout << endl;
 }
 
+class Point {
+public:
 
+   static Point newCartesianPoint( const float x, const float y ) {
+      return Point( x, y );
+   }
+
+   static Point newPolarPoint( const float radius, const float theta ) {
+      return Point( radius * cos(theta), radius * sin( theta ) );
+   }
+
+   friend std::ostream& operator<<(std::ostream& os, const Point & point)
+   {
+      return os << "P(" << point.x << ", " << point.y << ")";
+   }
+
+protected:
+
+   Point( const float x, const float y )
+         : x { x }, y { y } {
+   }
+
+   float x;
+   float y;
+};
 
 int main(int argc, char *argv[]) {
    printHeader();
+
+   Point cartesianPoint = Point::newCartesianPoint( 1.0, 2.0 );
+   std::cout << "cartesianPoint: " << cartesianPoint << std::endl;
+
+   Point polarPoint = Point::newPolarPoint( 1.0, M_PI_4f);
+   std::cout << "polarPoint: " << polarPoint << std::endl;
 
    cout << endl;
    return 0;
