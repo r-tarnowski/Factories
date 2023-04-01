78ab23d9d5bf2c16c3b976d0420c55fa57974f05
r-tarnowski <radek.github@wp.pl>
2023-04-01 18:30:05 +0200

    Factories axample completed

--- src/AbstractFactory.cpp
+++ src/AbstractFactory.cpp
@@ -15,12 +15,14 @@ struct HotDrink {
 struct Tea : HotDrink {
 
    void prepare(int volume) override {
+      cout << "Tea::prepare(" << volume << ")" << endl;
       cout << "Weź torebkę harbaty, zagotuj wodę, zalej " << volume << " ml, dodaj platerek cytryny." << endl;
    }
 };
 
 struct Coffee : HotDrink {
    void prepare(int volume) override {
+      cout << "Coffee::prepare(" << volume << ")" << endl;
       cout << "Zmiel ziarna, zagotuj wodę, zalej " << volume << " ml, dodaj śmietankę, smacznego!" << endl;
    }
 };
@@ -32,12 +34,14 @@ struct HotDrinkFactory {
 struct TeaFactory : HotDrinkFactory {
 
    std::unique_ptr<HotDrink> make() const override {
+      cout << "std::unique_ptr<HotDrink> TeaFactory::make()" << endl;
       return std::make_unique<Tea>();
    }
 };
 
 struct CoffeeFactory : HotDrinkFactory {
    std::unique_ptr<HotDrink> make() const override {
+      cout << "std::unique_ptr<HotDrink> CoffeeFactory::make()" << endl;
       return std::make_unique<Coffee>();
    }
 };
@@ -46,11 +50,14 @@ class DrinkFactory {
    std::map<std::string, std::unique_ptr<HotDrinkFactory>> hot_factories;
 public:
    DrinkFactory() {
+      cout << "DrinkFactory::DrinkFactory()" << endl;
       hot_factories["coffee"] = std::make_unique<CoffeeFactory>();
       hot_factories["tea"] = std::make_unique<TeaFactory>();
    }
 
    std::unique_ptr<HotDrink> make_drink( const std::string & name) {
+      cout << "std::unique_ptr<HotDrink> DrinkFactory:: make_drink("
+           << name << ")" << endl;
       auto drink = hot_factories[name]->make();
       drink->prepare(200); // ups!
       return drink;
@@ -63,10 +70,16 @@ class DrinkWithVolumeFactory {
 public:
 
    DrinkWithVolumeFactory() {
+      cout << "DrinkWithVolumeFactory::DrinkWithVolumeFactory()" << endl;
       factories["tea"] = [] {
          auto tea = std::make_unique<Tea>();
          tea->prepare(200);
          return tea;
+      };
+      factories["coffee"] = [] {
+         auto coffee = std::make_unique<Coffee>();
+         coffee->prepare(50);
+         return coffee;
       };
    }
 
@@ -74,10 +87,14 @@ public:
 };
 
 inline std::unique_ptr<HotDrink> DrinkWithVolumeFactory::make_drink(const std::string & name) {
+   cout << "std::unique_ptr<HotDrink> DrinkWithVolumeFactory::make_drink("
+        << name << ")" << endl;
    return factories[name]();
 }
 
 std::unique_ptr<HotDrink> make_drink(std::string type) {
+   cout << "std::unique_ptr<HotDrink> make_drink("
+        << type << ")" << endl;
    std::unique_ptr<HotDrink> drink;
    if (type == "tea") {
       drink = std::make_unique<Tea>();
@@ -102,13 +119,22 @@ void printHeader() {
 int main(int argc, char * argv[] ) {
    printHeader();
 
-   //Coffee coffee;
-   //Tea tea;
+   cout << "================================================" << endl;
+   cout << ">>> main: make_drink:" << std::endl;
+   auto d = make_drink("coffee");
+   cout << "================================================" << endl << endl;
 
-   auto d = make_drink("tea");
-
+   cout << "================================================" << endl;
+   cout << ">>> main: testing DrinkFactory:" << std::endl;
    DrinkFactory df;
    df.make_drink("coffee");
+   cout << "================================================" << endl << endl;
+
+   cout << "================================================" << endl;
+   cout << ">>> main: testing DrinkWithVolumeFactory:" << std::endl;
+   DrinkWithVolumeFactory dwvf;
+   dwvf.make_drink("coffee");
+   cout << "================================================" << endl << endl;
 
    return 0;
 }
